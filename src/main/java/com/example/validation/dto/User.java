faa531d0a0d28873eca4cc4afc08755109b68484
taekgyu <taekgyu0602@gmail.com>
2021-08-28 21:03:18 +0900

    @YearMonth 어노테이션 기능 직접 만들기

--- src/main/java/com/example/validation/dto/User.java
+++ src/main/java/com/example/validation/dto/User.java
@@ -1,9 +1,11 @@
 package com.example.validation.dto;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import com.example.validation.annotation.YearMonth;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 public class User {
 
@@ -18,6 +20,9 @@ public class User {
 
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "no form, 010-xxxx-xxxx") // 정규식 사용
     private String phoneNumber;
+
+    @YearMonth
+    private String reqYearMonth; // yyyyMM
 
     public String getName() {
         return name;
@@ -51,6 +56,20 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
+
+//    @AssertTrue(message = "yyyyMM 형식에 맞지 않습니다")
+//    public boolean isReqYearMonthValidation(){  // boolean 메서드에서 is 라는 키워드를 앞에 붙여줘야한다 ex)is메서드명
+//        System.out.println("~~~~");
+//
+//    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -58,6 +77,7 @@ public class User {
                 ", age=" + age +
                 ", email='" + email + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
                 '}';
     }
 }
